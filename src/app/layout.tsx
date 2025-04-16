c2171f31ddcde44d830a5e14abf402975a1d865b
Lietson Dos Santos <lietsondosantos.dev@gmail.com>
2025-04-16 01:55:02 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Kandala",
+  description: "Transforme Seu Cuidado Pessoal com Inteligência Digital",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <h1>Kandala</h1>
+    </>
+  );
+}
+
+export default Home
