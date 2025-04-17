b963df43ce10094464be5a41c2d0e0a94351ce28
Lietson Dos Santos <lietsondosantos.dev@gmail.com>
2025-04-17 13:29:13 +0100

    chore: installing sass and configuring global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+
+import './../styles/global.scss'
 
 export const metadata: Metadata = {
   title: 'Kandala - Landing Page',
   description: 'Transforme Seu Cuidado Pessoal com Inteligência Digital'
 }
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  display: 'swap',
+  variable: '--font-poppins'
+})
 
 export default function RootLayout({
   children
@@ -12,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='pt'>
+    <html lang='pt' className={poppins.variable}>
       <body>{children}</body>
     </html>
   )
