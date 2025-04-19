5784f5345563c3a245db1f55faf797f1a2e6b6fe
Lietson Dos Santos <lietsondosantos.dev@gmail.com>
2025-04-19 02:04:46 +0100

    feat: implement the Privacy Policy page

---
+++ src/app/legal/privacy/page.tsx
@@ -0,0 +1,108 @@
+import React from 'react'
+
+import styles from './styles.module.scss'
+
+function PolicyAndPrivacity() {
+  return (
+    <main className={styles['container']}>
+      <div className={styles['content']}>
+        <p className={styles['content__text']}>Informações</p>
+
+        <h1 className={styles['content__title']}>
+          Politica de privacidade do Kandala.
+        </h1>
+
+        <p className={styles['content__paragraph']}>
+          Use our built-in analytics dashboard to pull valuable insights and
+          monitor the value of your Krypto portfolio over time.
+        </p>
+
+        <p className={styles['content__paragraph']}>
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui
+          blanditiis{' '}
+          <span className={styles['paragraph__highlight']}>praesentium</span>{' '}
+          voluptatum deleniti atque corrupti quos dolores et quas molestias
+          excepturi sint occaecati cupiditate non provident, similique sunt in
+          culpa qui officia deserunt mollitia animi, i d est laborum et dolorum
+          fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam
+          libero tempore, cum soluta nobis est eligendi optio cumque nihil
+          impedit quo{' '}
+          <span className={styles['paragraph__highlight']}>minus</span> id quod
+          maxime placeat facere possimus, omnis voluptas assumenda est, omnis
+          dolor repellendus. Temporibus autem quibusdam et aut officiis debitis
+          aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae
+          sin t et molestiae non recusandae. Itaque earum rerum hic tenetur a
+          sapiente delectus, ut aut reiciendis voluptatibus maiores alias conseq
+          uatur aut perfer endis doloribus asperiores repellat.
+        </p>
+
+        <p className={styles['content__paragraph']}>
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui
+          blanditiis{' '}
+          <span className={styles['paragraph__highlight']}>praesentium</span>{' '}
+          voluptatum deleniti atque corrupti quos dolores et quas molestias
+          excepturi sint occaecati cupiditate non provident, similique sunt in
+          culpa qui officia deserunt mollitia animi, i d est laborum et dolorum
+          fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam
+          libero tempore, cum soluta nobis est eligendi optio cumque nihil
+          impedit quo{' '}
+          <span className={styles['paragraph__highlight']}>minus</span> id quod
+          maxime placeat facere possimus, omnis voluptas assumenda est, omnis
+          dolor repellendus. Temporibus autem quibusdam et aut officiis debitis
+          aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae
+          sin t et molestiae non recusandae. Itaque earum rerum hic tenetur a
+          sapiente delectus, ut aut reiciendis voluptatibus maiores alias conseq
+          uatur aut perfer endis doloribus.
+        </p>
+
+        <p className={styles['content__paragraph']}>
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui
+          blanditiis{' '}
+          <span className={styles['paragraph__highlight']}>praesentium</span>{' '}
+          voluptatum deleniti atque corrupti quos dolores et quas molestias
+          excepturi sint occaecati cupiditate non provident, similique sunt in
+          culpa qui officia deserunt mollitia animi, i d est laborum et dolorum
+          fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam
+          libero tempore, cum soluta nobis est eligendi optio cumque nihil
+          impedit quo{' '}
+          <span className={styles['paragraph__highlight']}>minus</span> id quod
+          maxime placeat facere possimus, omnis voluptas assumenda est, omnis
+          dolor repellendus. Temporibus autem quibusdam et aut officiis debitis
+          aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae
+          sin t et molestiae non recusandae. Itaque earum rerum hic tenetur a
+          sapiente delectus, ut aut reiciendis voluptatibus maiores alias conseq
+          uatur aut perfer endis doloribus.
+        </p>
+
+        <p className={styles['content__paragraph']}>
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui
+          blanditiis{' '}
+          <span className={styles['paragraph__highlight']}>praesentium</span>{' '}
+          voluptatum deleniti atque corrupti quos dolores et quas molestias
+          excepturi sint occaecati cupiditate non provident, similique sunt in
+          culpa qui officia deserunt mollitia animi, i d est laborum et dolorum
+          fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam
+          libero tempore, cum soluta nobis est eligendi optio cumque nihil
+          impedit quo{' '}
+          <span className={styles['paragraph__highlight']}>minus</span> id quod
+          maxime placeat facere possimus, omnis voluptas assumenda est, omnis
+          dolor repellendus. Temporibus autem quibusdam et aut officiis debitis
+          aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae
+          sin t et molestiae non recusandae. Itaque earum rerum hic tenetur a
+          sapiente delectus, ut aut reiciendis voluptatibus maiores alias conseq
+          uatur aut perfer endis doloribus.
+        </p>
+
+        <div className={styles['content__footer']}>
+          <p className={styles['footer__greetings']}>Seja bem-vindo(a)</p>
+          <p className={styles['footer__kandala']}>Kandala</p>
+          <p className={styles['footer__lastUpdate']}>
+            Última actualização em: 02/05/2024.
+          </p>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default PolicyAndPrivacity
